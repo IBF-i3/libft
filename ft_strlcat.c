95438a9d41d5b488d98e144b1e5cb5f25dcce42d
Iria Benavente Ferraces <ibenaven@c1r3s4.42madrid.com>
2024-10-01 07:49:32 +0200

    42madrid 3rd commit

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,30 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibenaven <ibenaven@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/01 05:41:06 by ibenaven          #+#    #+#             */
-/*   Updated: 2024/10/01 07:25:35 by ibenaven         ###   ########.fr       */
+/*   Created: 2024/10/01 07:36:45 by ibenaven          #+#    #+#             */
+/*   Updated: 2024/10/01 07:48:55 by ibenaven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	i;
-
-	i = 0;
-	if (size > 0)
-	{
-		while (src[i] != '\0' && i < (size - 1))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	return (ft_strlen(src));
+	
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ibenaven <ibenaven@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 01:13:24 by ibenaven          #+#    #+#             */
-/*   Updated: 2024/09/27 04:09:45 by ibenaven         ###   ########.fr       */
+/*   Updated: 2024/10/01 07:34:39 by ibenaven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,23 @@
 # include <stdio.h>
 # include <unistd.h>
 # include <string.h> 
+
+// Part 1
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+size_t		ft_strlen(const char *str);
+void		*ft_memset(void *s, int c, size_t n);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+// STRLCAT
+int			ft_toupper(int c);
+int			ft_tolower(int c);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
 
 #endif
