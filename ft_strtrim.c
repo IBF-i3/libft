bdd6d20423eb4d298b741ffd3bfd0037a2dde220
Iria Benavente Ferraces <ibenaven@c3r7s3.42madrid.com>
2024-10-27 23:00:55 +0100

    last commit

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: ibenaven <ibenaven@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/19 17:32:43 by ibenaven          #+#    #+#             */
-/*   Updated: 2024/10/27 22:14:18 by ibenaven         ###   ########.fr       */
+/*   Updated: 2024/10/27 22:51:58 by ibenaven         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,24 +27,3 @@ char	*ft_strtrim(char const *s1, char const *set)
 		s1_len--;
 	return (ft_substr(s1, i, s1_len - i));
 }
-/*
-int	main(void)
-{
-	char	*str;
-	char	*trim;
-	char	*str_trimmed;
-
-	str = "Hello my friend!";
-	trim = "my friend!";
-	str_trimmed = ft_strtrim(str, trim);
-	if (str_trimmed == NULL)
-	{
-		printf("Error: function returned NULL\n");
-		return (1);
-	}
-	printf("Original string: %s\n", str);
-	printf("Set to trim: %s\n", trim);
-	printf("Trimmed string: %s\n", str_trimmed);
-	free(str_trimmed);
-	return (0);
-}*/
